dc70f69328af00e2c003f2bd93a5fb91f23faed3
VLG-Klox <vlegac@klox.fr>
2021-07-07 11:42:39 +0200

    fc

--- src/App.js
+++ src/App.js
@@ -1,71 +1,116 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+	List,
+	CircularProgress,
+	IconButton,
+	Card,
+	CardMedia,
+	CardActionArea,
+	ImageListItemBar,
+	Backdrop,
+	Grid,
+	Box,
+	CardContent,
+	Typography,
+} from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-class Beer extends Component {
-  
-  render() {
-  return (
-     <div className="beerDiv">
-    <div style={{fontSize:'10pt'}}>{this.props.beer.name}</div>
-    <img src={this.props.beer.image_url} alt="" style={{width: '30px', float: 'left',paddingRight: '10px',paddingBottom: '10px'}}/>
-    <p style={{fontSize:'10pt'}}>{this.props.beer.tagline}</p>
-      </div>
-  )
-}
-}
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 class App extends Component {
- constructor(props) {
- super(props);
- 
- this.state = {
-      beers: [],
-      page: 1
-  }
-}
+	constructor(props) {
+		super(props);
 
-componentDidMount = () => {
-  window.addEventListener('scroll', this.infiniteScroll);
-  this.fetchData(this.state.page);
-}
+		this.state = {
+			koobs: [],
+			page: 1,
+			loading: false,
+		};
+	}
 
-infiniteScroll = () => {
-  // End of the document reached?
-  if (
-    window.innerHeight + document.documentElement.scrollTop
-    === document.documentElement.offsetHeight
-  ) 
-  {
-    let newPage = this.state.page;
-    newPage++;
-    this.setState({
-      page: newPage
-    });
-    
-    this.fetchData(newPage);
-  }
-}
+	componentDidMount = () => {
+		window.addEventListener('scroll', this.infiniteScroll);
+		this.fetchData(this.state.page);
+	};
 
-fetchData = (pageNum) => {
+	infiniteScroll = () => {
+		if (
+			window.innerHeight + document.documentElement.scrollTop ===
+			document.documentElement.offsetHeight
+		) {
+			let newPage = this.state.page;
+			newPage++;
+			this.setState({
+				page: newPage,
+				loading: true,
+			});
+			this.fetchData(newPage);
+		}
+	};
 
-  let beerUrl = 'https://api.punkapi.com/v2/beers?page='+pageNum;
+	fetchData = (pageNum) => {
+		let Url =
+			'https://koober.com/fr/api/koobs/categories/tous?orderby=&page=' +
+			pageNum;
 
-  fetch(beerUrl)
-  .then(res=>res.json())
-  .then(data => {
-    this.setState({
-      beers: [...this.state.beers,...data]
-    })
-  })
-}
+		fetch(Url)
+			.then((res) => res.json())
+			.then((data) => {
+				this.setState({
+					koobs: [...this.state.koobs, ...data],
+					loading: false,
+				});
+			});
+	};
 
-  render() {
-  return (
-    <div>
-     {this.state.beers.map((beerdata,idx) => (<Beer key={idx} beer={beerdata} />))}
-    </div>
-  );
-}
+	render() {
+		const { loading } = this.state;
+		return (
+			<Grid container justifyContent="space-between">
+				{this.state.koobs.map((koob, index) => {
+					return (
+						<Grid item xs={4} style={{ margin: '1rem 0' }} className={'flex'}>
+							<Card style={{ width: 300 }}>
+								<CardActionArea>
+									{!loading && koob.book.img_url.length > 0 ? (
+										<CardMedia
+											height={130}
+											component="img"
+											alt={`${koob.id}-${koob.book.writer}`}
+											title={koob.book.title}
+											image={koob.book.img_url}
+										/>
+									) : (
+										<Skeleton animation="wave" variant="rect" height={130} />
+									)}
+									<CardContent>
+										<Typography gutterBottom variant="caption">
+											Autheur:{' '}
+											<Typography
+												variant="body2"
+												color="textSecondary"
+												component="span"
+											>
+												{koob.book.writer}
+											</Typography>
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+							</Card>
+						</Grid>
+					);
+				})}
+				{this.state.loading && (
+					<Backdrop style={{ zIndex: 10 }} open={this.state.loading}>
+						<CircularProgress color="inherit" className="loader" />
+					</Backdrop>
+				)}
+			</Grid>
+		);
+	}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import App from './App';
+let logoImage =
+	'https://s3-us-west-2.amazonaws.com/koober/admin/koob/admin-60d326394c3a0.png';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<Logo />', () => {
+	it('renders an image', () => {
+		const logo = shallow(<App />);
+
+		expect(logo.find('img').prop('src')).toEqual(logoImage);
+	});
 });
